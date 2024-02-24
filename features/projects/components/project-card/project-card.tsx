8b2db3e7fb7f8be760426f531a7ec9d269f991ed
Anthony Tibbs <anthony@tibbs.ca>
2024-02-24 14:49:28 -0500

    Project list incorrect status (#7)
    * Add Cypress test to ensure correct project status labels are rendered
    
    * Fix ProjectStatus to match API, and display appropriate labels in project cards

--- features/projects/components/project-card/project-card.tsx
+++ features/projects/components/project-card/project-card.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import capitalize from "lodash/capitalize";
 import { Badge, BadgeColor } from "@features/ui";
 import { Routes } from "@config/routes";
 import { ProjectLanguage, ProjectStatus } from "@api/projects.types";
@@ -20,6 +19,12 @@ const statusColors = {
   [ProjectStatus.stable]: BadgeColor.success,
   [ProjectStatus.warning]: BadgeColor.warning,
   [ProjectStatus.critical]: BadgeColor.error,
+};
+
+const statusLabels = {
+  [ProjectStatus.stable]: "Stable",
+  [ProjectStatus.warning]: "Warning",
+  [ProjectStatus.critical]: "Critical",
 };
 
 export function ProjectCard({ project }: ProjectCardProps) {
@@ -50,7 +55,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
             <div className={styles.issuesNumber}>{numEvents24h}</div>
           </div>
           <div className={styles.status}>
-            <Badge color={statusColors[status]}>{capitalize(status)}</Badge>
+            <Badge color={statusColors[status]}>{statusLabels[status]}</Badge>
           </div>
         </div>
       </div>
